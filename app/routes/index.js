a3a6427843051c96e05ae8d15134e6243debe585
Michael Perry <perrymichaelscott@gmail.com>
2019-08-30 00:31:30 -0600

    add cors for twilio

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,6 +10,13 @@ var Room = require('../models/room');
 var Message = require('../models/message');
 
 var init = function (io) {
+
+	router.use(function (req, res, next) {
+		res.header("Access-Control-Allow-Origin", "https://www.twilio.com"); // update to match the domain you will make the request from
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		next();
+	});
+
 	// Home page
 	router.get('/', function (req, res, next) {
 		// If user is already logged in, then redirect to rooms page
